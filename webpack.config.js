bbd1075332d2b46fcb09a3acad1b68cd07a56f40
mt-snow <>
2021-05-13 23:37:14 +0900

    Chage output directory name from dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
       buldle: './src/index.js',
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: '[name].js'
     },
     devServer: {
